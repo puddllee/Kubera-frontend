92b187bbb1bdd47ff1a09be239becccc6cf19918
Tyson Battistella <tysonbattistella@gmail.com>
2018-01-06 20:17:46 -0800

    if the base component has no profile, try to fetch it before booting user to login

--- src/containers/baseContainer.js
+++ src/containers/baseContainer.js
@@ -2,6 +2,8 @@ import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
 import {push} from 'react-router-redux';
 import Base from 'components/base';
+import * as actions from "modules/auth/actions";
+import * as constants from "shared/constants";
 
 const mapStateToProps = (state) => {
   const {routing, auth, profile} = state;
@@ -11,8 +13,16 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     onMount: (auth) => {
+      // If there is no profile loaded,
+      // but there is a token, try to
+      // fetch the profile with the token.
+      // Otherwise, just log out
       if(!auth.profile.id){
-        dispatch(push('/login'));
+        if (window.localStorage.getItem(constants.API_STORAGE_KEY)){
+          dispatch(actions.getProfile())
+        } else {
+          dispatch(push('/login'));
+        }
       }
     }
   };
