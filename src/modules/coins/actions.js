6bf78f2a0b3e93857e7f6a197c3f181caf34d015
Jake Runzer <jakerunzer@gmail.com>
2018-01-19 17:54:00 -0800

    add sparkline route

--- src/modules/coins/actions.js
+++ src/modules/coins/actions.js
@@ -8,46 +8,55 @@ export const GET_COIN_HISTORY_SUCCESS = 'coins/GET_COIN_HISTORY_SUCCESS';
 export const GET_COIN_HISTORY_FAILURE = 'coins/GET_COIN_HISTORY_FAILURE';
 export const SET_SELECTED_COIN_SYMBOLS = 'coins/SET_SELECTED_COIN_SYMBOLS';
 
-export function getCoins(){
-  return (dispatch) => {
+export function getCoins() {
+  return dispatch => {
     dispatch({
       types: [GET_COINS, GET_COINS_SUCCESS, GET_COINS_FAILURE],
-      promise: (client) => {
-        return client.get(routes.coins.allCoins())
-          .then((resp)=>{
-            dispatch(getCoinHistory(resp[0].symbol));
-            return resp;
-          })
+      promise: client => {
+        return client.get(routes.coins.allCoins()).then(resp => {
+          dispatch(getCoinHistory(resp[0].symbol));
+          return resp;
+        });
       }
-    })
-  }
+    });
+  };
 }
 
-export function getCoinHistory(symbol){
+export function getCoinHistory(symbol) {
   return (dispatch, getState) => {
     // Only get the coin history if it does not yet exist in memory
-    if(!getState().coins.histories[symbol]){
+    if (!getState().coins.histories[symbol]) {
       dispatch({
-        types: [GET_COIN_HISTORY, GET_COIN_HISTORY_SUCCESS, GET_COIN_HISTORY_FAILURE],
-          promise: (client) => {
-            // https://github.com/CoinCapDev/CoinCap.io#history1daycoin
-            const freq = '1day'; // 1, 7, 30, 90, 180, 365
-            return client.get(routes.coins.coinHistory(freq, symbol));
+        types: [
+          GET_COIN_HISTORY,
+          GET_COIN_HISTORY_SUCCESS,
+          GET_COIN_HISTORY_FAILURE
+        ],
+        promise: client => {
+          // https://github.com/CoinCapDev/CoinCap.io#history1daycoin
+          const freq = '30day'; // 1, 7, 30, 90, 180, 365
+          return client.get(routes.coins.coinHistory(freq, symbol));
         }
-      })
+      });
     }
-  }
+  };
 }
 
-export function toggleCoinSelect(symbol){
+export function toggleCoinSelect(symbol) {
   return (dispatch, getState) => {
     const coins = getState().coins;
-    const {selectedCoinSymbols} = coins;
+    const { selectedCoinSymbols } = coins;
 
     if (selectedCoinSymbols.includes(symbol)) {
-      dispatch({type: SET_SELECTED_COIN_SYMBOLS, payload: selectedCoinSymbols.filter((s)=>s!==symbol)});
+      dispatch({
+        type: SET_SELECTED_COIN_SYMBOLS,
+        payload: selectedCoinSymbols.filter(s => s !== symbol)
+      });
     } else {
-      dispatch({type: SET_SELECTED_COIN_SYMBOLS, payload: [...selectedCoinSymbols, symbol]});
+      dispatch({
+        type: SET_SELECTED_COIN_SYMBOLS,
+        payload: [...selectedCoinSymbols, symbol]
+      });
     }
   };
 }
